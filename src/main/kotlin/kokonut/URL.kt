883071f77f81a768d82c207ba72f165cfa704209
volta2030 <volta2030@gmail.com>
2024-08-17 01:23:46 +0900

    feat : Add new FULL_NODE

--- src/main/kotlin/kokonut/URL.kt
+++ src/main/kotlin/kokonut/URL.kt
@@ -4,5 +4,6 @@ object URL{
     const val FUEL_NODE = "https://pascal-institute.github.io/kokonut-oil-station/"
     const val FULL_STORAGE = "https://api.github.com/repos/Pascal-Institute/kokonut-storage/contents/"
     const val FULL_RAW_STORAGE ="https://raw.githubusercontent.com/Pascal-Institute/kokonut-storage/main/"
-    const val FULL_NODE_0 = "http://kokonut.iptime.org"
+    const val FULL_NODE_0 = "https://kokonut.koyeb.app"
+    const val FULL_NODE_1 = "http://kokonut.iptime.org"
 }
