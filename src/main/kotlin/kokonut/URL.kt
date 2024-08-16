5fca859584df0831df4bc9e3a1f6f5bd66d67e75
volta2030 <volta2030@gmail.com>
2024-08-16 21:28:07 +0900

    fix : add const to variables

--- src/main/kotlin/kokonut/URL.kt
+++ src/main/kotlin/kokonut/URL.kt
@@ -1,8 +1,8 @@
 package kokonut
 
 object URL{
-    val FUEL_NODE = "https://pascal-institute.github.io/kokonut-oil-station/"
-    val FULL_STORAGE = "https://api.github.com/repos/Pascal-Institute/kokonut-storage/contents/"
-    val FULL_RAW_STORAGE ="https://raw.githubusercontent.com/Pascal-Institute/kokonut-storage/main/"
-    val FULL_NODE_0 = "http://kokonut.iptime.org"
+    const val FUEL_NODE = "https://pascal-institute.github.io/kokonut-oil-station/"
+    const val FULL_STORAGE = "https://api.github.com/repos/Pascal-Institute/kokonut-storage/contents/"
+    const val FULL_RAW_STORAGE ="https://raw.githubusercontent.com/Pascal-Institute/kokonut-storage/main/"
+    const val FULL_NODE_0 = "http://kokonut.iptime.org"
 }
