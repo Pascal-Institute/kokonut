b759629d31343bdea6a6b931381006f2c9a5954e
volta2030 <volta2030@gmail.com>
2024-08-20 09:30:26 +0900

    Version Up

--- build.gradle.kts
+++ build.gradle.kts
@@ -35,7 +35,7 @@ publishing {
             groupId = "com.pascal.institute" //Navigate beyond computing oceans
             artifactId = "kokonut"
 
-            version = "1.0.8"
+            version = "1.0.9"
 
             from(components["java"])
         }
