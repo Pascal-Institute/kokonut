1f182124d48935dfd43c62d5086d049ccbbcd63e
volta2030 <volta2030@gmail.com>
2024-08-24 12:59:11 +0900

    fix Data class : for change reward in calculateHash...

--- src/main/kotlin/kokonut/core/Data.kt
+++ src/main/kotlin/kokonut/core/Data.kt
@@ -4,7 +4,7 @@ import kotlinx.serialization.Serializable
 
 @Serializable
 data class Data(
-    val reward: Double,
+    var reward: Double,
     val ticker: String = Identity.ticker,
     val miner: String,
     val transactions : List<Transaction>?,
