29dfac778cd47ea69c611cb5a1ce2779b3238c30
volta2030 <volta2030@gmail.com>
2024-08-25 09:28:26 +0900

    fix : modify FULL_RAW_STORAGE

--- src/main/kotlin/kokonut/URL.kt
+++ src/main/kotlin/kokonut/URL.kt
@@ -3,7 +3,7 @@ package kokonut
 object URL{
     const val FUEL_NODE = "https://pascal-institute.github.io/kokonut-oil-station/"
     const val FULL_STORAGE = "https://api.github.com/repos/Pascal-Institute/kovault/contents/"
-    const val FULL_RAW_STORAGE ="https://raw.githubusercontent.com/Pascal-Institute/kokonut-kovault/main/"
+    const val FULL_RAW_STORAGE ="https://raw.githubusercontent.com/Pascal-Institute/kovault/main/"
     const val FULL_NODE_0 = "https://kokonut.koyeb.app"
     const val FULL_NODE_1 = "http://kokonut.iptime.org"
 }
