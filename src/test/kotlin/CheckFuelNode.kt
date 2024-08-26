f192e94620401d0b4aa1ad6fc0d4973da4d93f4c
volta2030 <volta2030@gmail.com>
2024-08-26 22:38:13 +0900

    hotfix in version 4.0.1

--- src/test/kotlin/CheckFuelNode.kt
+++ src/test/kotlin/CheckFuelNode.kt
@@ -1,8 +1,8 @@
 import kokonut.URLBook.FUEL_NODE
-import kokonut.util.API.Companion.sendHttpGetPolicy
+import kokonut.util.API.Companion.getPolicy
 
 fun main() {
-    val policy = FUEL_NODE.sendHttpGetPolicy()
+    val policy = FUEL_NODE.getPolicy()
     println(policy.version)
     println(policy.difficulty)
 }
