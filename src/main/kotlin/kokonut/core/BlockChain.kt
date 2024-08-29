18d92830ea426cb0cf058a86c4141e30fd4e40e1
volta2030 <volta2030@gmail.com>
2024-08-29 22:57:27 +0900

    modify init{} syncChain() out Of runBlocking

--- src/main/kotlin/kokonut/core/BlockChain.kt
+++ src/main/kotlin/kokonut/core/BlockChain.kt
@@ -34,9 +34,9 @@ class BlockChain(val node: Node = Node.LIGHT, val url: URL = URLBook.FULL_NODE_0
                 Node.FULL -> loadChainFromFuelNode()
                 else -> loadChainFromFullNode()
             }
-            syncChain()
             println("Block Chain validity : ${isValid()}")
         }
+        syncChain()
     }
 
     suspend fun loadChainFromFuelNode() {
