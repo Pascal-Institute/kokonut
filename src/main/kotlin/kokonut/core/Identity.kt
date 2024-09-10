1f9e1cc50b1b9ad76d49dd50a053dd1edede6954
volta2030 <volta2030@gmail.com>
2024-09-10 22:48:48 +0900

    modify Identity... : using properties

--- src/main/kotlin/kokonut/core/Identity.kt
+++ src/main/kotlin/kokonut/core/Identity.kt
@@ -1,10 +1,6 @@
 package kokonut.core
 
-import kokonut.util.Utility.Companion.findKokonutJar
-import java.io.File
-import java.io.InputStream
 import java.util.*
-import java.util.jar.Manifest
 
 object Identity {
     const val majorIndex = 0
@@ -12,39 +8,12 @@ object Identity {
     var isRegistered = false
 
     private val properties: Properties = Properties().apply {
-        Identity.javaClass.classLoader.getResourceAsStream("mode.properties")?.use { load(it) }
-    }
-
-    private val mode = properties.getProperty("mode", "release")
-
-    private val manifest: Manifest by lazy {
-
-        println(mode)
-
-        val manifestStream: InputStream? = when(mode){
-            "release" ->{
-                this::class.java.classLoader.getResourceAsStream("META-INF/MANIFEST.MF")
-            }
-            "develop"->{
-                val jarFile = File(findKokonutJar())
-                if (jarFile.exists()) {
-                    val jar = java.util.jar.JarFile(jarFile)
-                    jar.getInputStream(jar.getEntry("META-INF/MANIFEST.MF"))
-                } else {
-                    throw IllegalStateException("Manifest file not found in debugging environment")
-                }
-            }
-
-            else -> {
-                throw IllegalStateException("Manifest file not found in debugging environment")
-            }
-        }
-        Manifest(manifestStream)
+        Identity.javaClass.classLoader.getResourceAsStream("kokonut.properties")?.use { load(it) }
     }
 
     val libraryVersion : String
-        get() = manifest.mainAttributes.getValue("Implementation-Version")
+        get() = properties.getProperty("version", "0.0.0")
 
     val protocolVersion: Int
-    get() = libraryVersion.split(".")[majorIndex].toInt()
+        get() = libraryVersion.split(".")[majorIndex].toInt()
 }
