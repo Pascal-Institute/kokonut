e1d9986f5e5553a290e015f42f5103ed54bc7341
volta2030 <volta2030@gmail.com>
2024-09-14 17:04:00 +0900

    fix : remove insert() & syncChain in mine()
    lightnode kovault will be added after add into fullnode kovault

--- src/main/kotlin/kokonut/core/BlockChain.kt
+++ src/main/kotlin/kokonut/core/BlockChain.kt
@@ -165,10 +165,6 @@ class BlockChain(val node: Node = Node.LIGHT, val url: URL = URLBook.FULL_NODE_0
             println("Nonce : $nonce")
         }
 
-        database.insert(miningBlock)
-
-        syncChain()
-
         return miningBlock
     }
 
