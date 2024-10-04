53ab84837edc5859d27073e5ab04e809e0afefb8
volta2030 <volta2030@gmail.com>
2024-10-05 08:00:02 +0900

    fix: include fullnode in settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
+
 include("library")
-//Disable for kokonut library jitpack build...
-//include("fullnode")
+include("fullnode")
