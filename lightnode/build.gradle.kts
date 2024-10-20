a7178b60f10d8c7c8fecaa57f1fa0ed7675a7daa
volta2030 <volta2030@gmail.com>
2024-10-20 13:22:26 +0900

    fix: stablization gradle build for kokonut framework...

--- lightnode/build.gradle.kts
+++ lightnode/build.gradle.kts
@@ -1,14 +1,15 @@
 import org.jetbrains.compose.desktop.application.dsl.TargetFormat
 
 plugins {
-    kotlin("jvm")
-    id("org.jetbrains.compose")
+    kotlin("jvm") version "1.9.22"
+    id("org.jetbrains.compose") version "1.6.0"
 }
 
 group = "io.github.pascal-institute"
 version = "1.0-SNAPSHOT"
 
 repositories {
+    gradlePluginPortal()
     mavenCentral()
     maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     google()
