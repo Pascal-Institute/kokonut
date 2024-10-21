5823a8ff33137b7eff869b02d9ece62ecc66fb9c
volta2030 <volta2030@gmail.com>
2024-10-21 23:45:02 +0900

    fix: remove unused libs in fullnode

--- fullnode/build.gradle.kts
+++ fullnode/build.gradle.kts
@@ -25,11 +25,9 @@ dependencies {
     implementation("io.ktor:ktor-server-html-builder:2.3.4")
     implementation("io.ktor:ktor-server-core-jvm:2.3.4")
     implementation("io.ktor:ktor-server-netty-jvm:2.3.4")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.8.1")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:2.3.4")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:2.3.4")
     implementation("ch.qos.logback:logback-classic:1.4.12")
-    implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.8.0")
 }
 
 tasks.named("build") {
