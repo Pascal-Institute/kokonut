30dd9b4114309f7562f99f1217acfd0492b04a86
volta2030 <volta2030@gmail.com>
2024-10-26 18:52:17 +0900

    fix: setting to production mode

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 rootProject.name = "kokonut"
 
 //develop
-//include(":library")
+include(":library")
 
 //production
-include(":fuelnode", ":fullnode", ":lightnode", ":library")
+//include(":fuelnode", ":fullnode", ":lightnode", ":library")
