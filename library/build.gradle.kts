cc4414b21c641f4c8b1c661d5db62df01c52509d
volta2030 <volta2030@gmail.com>
2024-10-27 21:56:21 +0900

    fix: specified jvm and fix plugn.serialization version

--- library/build.gradle.kts
+++ library/build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
     java
-    kotlin("jvm")
-    kotlin("plugin.serialization") version "1.9.22"
+    kotlin("jvm") version "1.9.22"
+    kotlin("plugin.serialization") version "1.8.20"
     `maven-publish`
 }
 
